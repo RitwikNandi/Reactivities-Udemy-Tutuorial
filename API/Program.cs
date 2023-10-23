1b68694a0c31c3e1664e9daef81ba059a88d126d
RitwikNandi <ritwiknandi006@gmail.com>
2023-10-23 18:47:05 +0530

    API and Client walking skeleton is ready

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+	opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors(opt =>
+{
+	opt.AddPolicy("CorsPolicy", policy =>
+	{
+		policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+	});
 });
 
 var app = builder.Build();
@@ -18,11 +25,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
@@ -33,15 +42,15 @@ var services = scope.ServiceProvider;
 
 try
 {
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+	var context = services.GetRequiredService<DataContext>();
+	await context.Database.MigrateAsync();
+	await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
-    
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
+	
+	var logger = services.GetRequiredService<ILogger<Program>>();
+	logger.LogError(ex, "An error occured during migration");
 
 }
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,31 @@
-import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:9000/api/activities/").then((response) => {
+      setActivities(response.data);
+    });
+    // console.log(
+    //   activities.map((activity: any) => {
+    //     return <h1 key={activity.id}>{activity.title}</h1>;
+    //   })
+    // );
+  }, []);
+
   return (
     <>
-      <h1>reactivities</h1>
+      <Header as='h2' iicon='users' content='Reactivities' />
+      <div>
+        <List>
+          {activities.map((activity: any) => {
+            return <List.Item key={activity.id}>{activity.title}</List.Item>;
+          })}
+        </List>
+      </div>
     </>
   );
 }
